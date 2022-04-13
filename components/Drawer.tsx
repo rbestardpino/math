6aea524b5933403f83883a63c3863e801b56b39d
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2022-04-12 22:22:54 -0300

    drawer

---
+++ components/Drawer.tsx
@@ -0,0 +1,56 @@
+import topics from "@lib/topics";
+import HomeIcon from "@mui/icons-material/Home";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import _Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Image from "next/image";
+import Link from "next/link";
+import { MouseEventHandler } from "react";
+
+interface Props {
+  open: boolean;
+  handleClose: MouseEventHandler;
+}
+
+export default function Drawer({ open, handleClose }: Props) {
+  return (
+    <_Drawer open={open} onClose={handleClose}>
+      <Box sx={{ width: 250 }} role="presentation">
+        <List>
+          <Link href="/" passHref>
+            <ListItem sx={{ my: 1 }} button onClick={handleClose}>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="HOME"
+                primaryTypographyProps={{ fontWeight: "bold" }}
+              />
+            </ListItem>
+          </Link>
+          <Divider />
+          {topics.map((topic) => (
+            <>
+              <Link href={topic.url} passHref>
+                <ListItem button key={topic.name} onClick={handleClose}>
+                  <ListItemIcon>
+                    <Image src={topic.img} height={30} width={30} />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={topic.name}
+                    primaryTypographyProps={{ noWrap: true }}
+                  />
+                </ListItem>
+              </Link>
+              <Divider />
+            </>
+          ))}
+        </List>
+      </Box>
+    </_Drawer>
+  );
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,32 @@
+import Drawer from "@components/Drawer";
+import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
+import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
   const { asPath } = useRouter();
+
+  const toggleDrawer =
+    (open: boolean) => (event: KeyboardEvent | MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as KeyboardEvent).key === "Tab" ||
+          (event as KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setOpenDrawer(open);
+    };
 
   const titles = asPath.split("/");
   titles.shift();
@@ -18,9 +37,17 @@ export default function Navbar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
+          <IconButton
+            color="inherit"
+            onClick={() => setOpenDrawer(true)}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer open={openDrawer} handleClose={() => setOpenDrawer(false)} />
           <Breadcrumbs aria-label="breadcrumb">
             <Link underline="hover" color="inherit" href="/">
-              HOME
+              Home
             </Link>
             {titles.map((title) => {
               const path = asPath.slice(0, asPath.indexOf(title)) + title;
